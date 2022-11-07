0d2e4275ec278deaacbcbf5ef0c17d9660b960a7
Andrew <93141508+svpra@users.noreply.github.com>
2022-11-07 23:22:04 +0300

    Update Type.cs

--- first/Type.cs
+++ first/Type.cs
@@ -1,6 +1,3 @@
-/* Так как комментарии улучшают читаемость кода, а также это good practice, 
-всё в коде будет тщательно закомментировано :) */
-
 using System;
 using System.Math;
 using System.Collections.Generic;
@@ -9,19 +6,15 @@ using System.Text;
 
 namespace MindBoxLibrary
 {
-    // создаём абстрактный класс, который будет определять тип фигуры
     public abstract class Type
     {
         public string FigureName { get; set; }
 
         public Type(string FigureName)
 
-        // CalcSquare - абстрактный метод, который мы переопределим позже, 
-        // так как для классов Circle и Triangle будут разные реализации этого метода
         public abstract double CalcSquare();
     }
 
-    // Основной класс
     public static class Prototype
     {
         public static double CalcSquare(Type type) => type.CalcSquare();    
