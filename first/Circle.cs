96ead18818949f236cfe8a7cd2b0a871f3196390
Andrew <93141508+svpra@users.noreply.github.com>
2022-11-07 23:24:06 +0300

    Update Circle.cs

--- first/Circle.cs
+++ first/Circle.cs
@@ -7,15 +7,12 @@ using MindBoxLibrary;
 
 namespace MindBoxLibrary
 {
-    // создаём класс Circlе
     public class Circle : Type
     {
         public double Radius { get; set; }
 
-        // Создаём круг
         public Circle(string FigureName, double Radius) : base(FigureName)
         {
-            // Проверяем: отрицательный ли радиус
             if (Radius < 0) 
             {
                 throw new ArgumentException("Ошибка: значение стороны не может быть отрицательным");
@@ -26,7 +23,6 @@ namespace MindBoxLibrary
             }
         }
 
-        // Считаем площадь
         public override double CalcSquare()
         {
             return Math.Round(Math.PI * Math.Pow(Radius, 2), 1);
