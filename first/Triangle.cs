c4fe51b0b23ba4c8b734eb3abad464af5d047c5d
Andrew <93141508+svpra@users.noreply.github.com>
2022-11-07 23:23:17 +0300

    Update Triangle.cs

--- first/Triangle.cs
+++ first/Triangle.cs
@@ -7,15 +7,14 @@ using MindBoxLibrary.Type;
 
 namespace MindBoxLibrary {
 
-    // Создаём класс Triangle
+    
     public class Triangle : Type
     {
-        // Инициализируем стороны
+        
         public double ab { get; set; }
         public double bc { get; set; }
         public double ac { get; set; }
 
-        // Создаём треугольник (и проверяем: является ли треугольником то, что пытается создать пользователь)
         public Triangle(string FigureName, double ab, double bc, double ac) : base(FigureName)
         {
             if (ab < 0 or bc < 0 or ac < 0) {
@@ -34,16 +33,13 @@ namespace MindBoxLibrary {
             }
         }
 
-        // Создаём метод CalcSquare, который будет считать площадь
         public override double CalcSquare()
         {
-            // Считаем площадь по формуле сумма сторон 
             double p = (ab + bc + ac) / 2;
             double result = Math.Round(Math.Sqrt(p * (p - ab) * (p - bc) * (p - ac)), 1);
             return result;
         }
 
-        // Проверяем: является ли треугольник прямоугольным
         public bool IsRectangular()
         {
             bool check = (ab == Math.Sqrt(Math.Pow(bc, 2) + Math.Pow(ac, 2))
